e424090b160ab7fd569a91fcad4e85ed2e3fec7f
Luca Dal Mas <luca.dalmas@icloud.com>
2023-05-01 16:01:43 +0200

    Initial Commit

---
+++ NW/NWApp.swift
@@ -0,0 +1,17 @@
+//
+//  NWApp.swift
+//  NW
+//
+//  Created by Luca Dal Mas on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct NWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
